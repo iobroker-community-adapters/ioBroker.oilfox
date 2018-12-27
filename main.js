69024681e8037835db21355e5a746743094c7733
jkuehner <jochen.kuehner@kardex.com>
2018-12-27 16:40:23 +0100

    create all properties

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const querystring = require('querystring');
 const https        = require('https');
 
 let pollInterval;
+let pollTimer;
 
 adapter.on('message', function (obj) {
     adapter.log.debug("adapter.on-message: << MESSAGE >>");
@@ -28,6 +29,9 @@ adapter.on('ready', function () {
 
 adapter.on('unload', function () {
     adapter.log.debug("adapter.on-unload: << UNLOAD >>");
+	
+	if (pollTimer)
+		clearTimeout(pollTimer);
 });
 
 adapter.on('stateChange', function (id, state) {
@@ -35,10 +39,10 @@ adapter.on('stateChange', function (id, state) {
 });
 
 
-function connectOilfox(email, password) {
+function connectOilfox() {
 	let post_data = JSON.stringify({
-      'email' : email,
-      'password': password
+      'email' : adapter.config.email,
+      'password': adapter.config.password
     });
 	
 	let post_options = {
@@ -70,7 +74,61 @@ function connectOilfox(email, password) {
 				 let result = JSON.parse(chunk2);
 				 const promises = [];
 
-				promises.push(adapter.setObjectNotExistsAsync('info.country', {
+				for (let p in result)
+				{
+					if (typeof result[p] !== 'object')
+					{
+						promises.push(adapter.setObjectNotExistsAsync('info.' + p, {
+							type: 'state',
+							common: {
+								'name': 'Display content 0',
+								'role': 'info.display',
+								'type': 'string',
+								'write': false,
+								'read': true
+							},
+							native: {}
+						}));
+					}
+				}
+				let i = 0;
+				for (let p in result.devices)
+				{
+					if (typeof result[p] !== 'object')
+					{
+						promises.push(adapter.setObjectNotExistsAsync('devices.' + i + '.' + p, {
+							type: 'state',
+							common: {
+								'name': 'Display content 0',
+								'role': 'info.display',
+								'type': 'string',
+								'write': false,
+								'read': true
+							},
+							native: {}
+						}));
+					}
+					
+					for (let pp in result.devices[i])
+					{
+						if (typeof result[pp] !== 'object')
+						{
+							promises.push(adapter.setObjectNotExistsAsync('devices.' + i + '.metering.' + pp, {
+								type: 'state',
+								common: {
+									'name': 'Display content 0',
+									'role': 'info.display',
+									'type': 'string',
+									'write': false,
+									'read': true
+								},
+								native: {}
+							}));
+						}
+					}
+					i++;
+				}
+				/*promises.push(adapter.setObjectNotExistsAsync('info.country', {
 					type: 'state',
 					common: {
 						'name': 'Display content 0',
@@ -80,9 +138,9 @@ function connectOilfox(email, password) {
 						'read': true
 					},
 					native: {}
-				}));
+				}));*/
 				
-				let i = 0;
+				/*let i = 0;
 				for (let d of result.devices) {
 					promises.push(adapter.setObjectNotExistsAsync('devices.' + i + '.id', {
 					type: 'state',
@@ -108,15 +166,41 @@ function connectOilfox(email, password) {
 					native: {}
 					}));
 					i++;
-				}
+				}*/
 				Promise.all(promises).then(() => {
-					adapter.setState('info.country', result.country, true);
+					for (let p in result)
+					{
+						if (typeof result[p] !== 'object')
+						{
+							adapter.setState('info.' + p, result[p], true);
+						}
+					}
 					let j = 0;
+					for (let p in result.devices)
+					{
+						if (typeof result[p] !== 'object')
+						{
+							adapter.setState('devices.' + j + '.' + p, result.devices[0][p], true);
+						}
+						
+						for (let pp in result.devices[i])
+						{
+							if (typeof result[pp] !== 'object')
+							{
+								adapter.setState('devices.' + j + '.metering.' + pp, result.devices[0].metering[pp], true);								
+							}
+						}
+						j++;
+					}
+					//adapter.setState('info.country', result.country, true);
+					/*let j = 0;
 					for (let d of result.devices) {
 						adapter.setState('devices.' + j + '.id', result.devices[0].id, true);
 						adapter.setState('devices.' + j + '.metering.liters', result.devices[0].metering.liters, true);
 						j++;
-					}					
+					}*/
+
+					pollTimer = setTimeout(() => connectOilfox(), adapter.config.pollInterval ?  adapter.config.pollInterval : 60000);					
 					
 				});
 			});
@@ -135,8 +219,5 @@ function connectOilfox(email, password) {
 function main() {
     adapter.log.debug("adapter.main: << MAIN >>");
 	
-	var email = adapter.config.email;
-	var password = adapter.config.password;
-	
-    connectOilfox(email, password);
+	connectOilfox();
 }
